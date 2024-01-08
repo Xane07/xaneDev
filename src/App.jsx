bd98b41089d3d8a57c493b61c9b4c4cbf05c08eb
Pratham Pachori <92997073+Xane07@users.noreply.github.com>
2024-01-08 18:36:51 +0530

    8 jan

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import Tab from "./elements/Tab.jsx";
-import Header from "./elements/header.jsx";
+import Header from "./elements/header/header.jsx";
+import Text from './elements/introduction/Text.jsx'
 
 
 function App() {
   return (
     <>
       <Header />
+      <Text />
+      <div style={{height : "800px"}}></div>
     </>
   );
 }
---
+++ src/elements/introduction/Text.jsx
@@ -0,0 +1,15 @@
+import "./text.css";
+
+function Text() {
+
+  console.log("text.jsx");
+  return (
+    <div className="textContainer">
+      <h3 className="text">
+        Hi, I'm Pratham aka <h3 className="text purple">Xane</h3> and I am a
+      </h3>
+    </div>
+  );
+}
+
+export default Text;
