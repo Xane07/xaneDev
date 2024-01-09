0a532593fb931e791db1dd605ca660709c60e0c8
Pratham Pachori <92997073+Xane07@users.noreply.github.com>
2024-01-09 22:43:42 +0530

    cursor added

--- src/elements/introduction/Text.jsx
+++ src/elements/introduction/Text.jsx
@@ -1,13 +1,23 @@
 import "./Text.css";
+import { useTypewriter , Cursor } from 'react-simple-typewriter'
 
 function Text() {
-
+  const {text} = useTypewriter({
+    words: ["Youtuber.", "freelancer."],
+    loop: {},
+    typeSpeed: 120,
+    deleteSpeed: 40
+  })
   console.log("text.jsx");
   return (
     <div className="textContainer">
       <h3 className="text">
-        Hi, I'm Pratham aka <h3 className="text purple">Xane</h3> and I am a
+        Hi, I'm Pratham aka <h3 className="text purple">Xane</h3> and I am a 
       </h3>
+        <div>
+          <span className="text2">{text}</span>
+          <span className="cursor"><Cursor /></span>
+        </div>
     </div>
   );
 }
