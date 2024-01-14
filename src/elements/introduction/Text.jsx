f98f55b443b2c8ce11de1476093db3505ccfc4c4
Pratham Pachori <92997073+Xane07@users.noreply.github.com>
2024-01-14 13:25:31 +0530

    font awesome

--- src/elements/introduction/Text.jsx
+++ src/elements/introduction/Text.jsx
@@ -1,27 +1,41 @@
 import React from "react";
 import "./Text.css";
 import Typewriter from "typewriter-effect";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Text() {
   console.log("text.jsx");
   return (
-    <div className="textContainer">
-      <h3 className="text">
-        Hi, I'm Pratham aka <span className="text purple">Xane</span> and I am a
-      </h3>
-      <div>
-        <span className="text2"></span>
-        <span className="cursor">
-          <Typewriter
-            options={{
-              strings: ["Coder.", "Developer.","Youtuber.","Learner."],
-              autoStart: true,
-              loop: true,
-            }}
-          />
-        </span>
+    <>
+      <div className="textContainer">
+        <h3 className="text">
+          Hi, I'm Pratham aka <span className="text purple">Xane</span> and I am
+          a
+        </h3>
+        <div>
+          <span className="text2"></span>
+          <span className="cursor">
+            <Typewriter
+              options={{
+                strings: ["Coder.", "Developer.", "Youtuber.", "Learner."],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </span>
+        </div>
       </div>
-    </div>
+      <div className="skills">
+        <p>Tech Stack |</p>
+        <div className="icons">
+        <FontAwesomeIcon icon="fa-brands fa-html5" />
+        <FontAwesomeIcon icon="fa-brands fa-html5" />
+        <FontAwesomeIcon icon="fa-brands fa-js" />
+        <FontAwesomeIcon icon="fa-brands fa-node" />
+        <FontAwesomeIcon icon="fa-brands fa-react" />
+        </div>
+      </div>
+    </>
   );
 }
 
