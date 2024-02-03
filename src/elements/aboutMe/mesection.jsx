da8752c7fe3b5c2144729020ceecaaf005b95a79
Xane07 <xanegmingyt@gmail.com>
2024-02-04 00:38:41 +0530

    edited about me section

--- src/elements/aboutMe/mesection.jsx
+++ src/elements/aboutMe/mesection.jsx
@@ -10,15 +10,51 @@ function Me() {
           </h3>
           <h5 className="subhd">about me:</h5>
           <p className="para">
-            I'm a passionate software dev with a background in BCA + MCA Degree
+            {/* I'm a passionate software dev with a background in BCA + MCA Degree
             ( in progress ) from Amity University , and I am excited about
             creating stunning websites and project. My expertise lies in
             translating designs into fully functional websites while utilizing
             my development skills to enhance and customize their features.
             <br></br>
             <br></br>In my leisure time, I enjoy learning new skill and
-            technologies while sometimes I play games to freshen up my mind.
+            technologies while sometimes I play games to freshen up my mind. */}
+            Hello! I'm Pratham Pachori, a dedicated student. My passion lies in
+            crafting innovative solutions through the world of software
+            development.
           </p>
+          <h5 className="subhd">educational background:</h5>
+          <p className="para">
+            Currently enrolled in the BCA + MCA program at Amity University,
+            Noida.
+          </p>
+          <h5 className="subhd">skills:</h5>
+          <p className="para">
+            <li>
+              Proficient in JavaScript, Java, Python, HTML, CSS, and React.js.
+            </li>
+            <li>
+              Enthusiastic about utilizing technology to solve real-world
+              problems.
+            </li>
+          </p>
+          <h5 className="subhd">continuous learning:</h5>
+          <p className="para">
+            A fervent learner, always seeking opportunities for skill
+            enhancement and staying abreast of the latest industry trends.
+          </p>
+          <h5 className="subhd">let's connect:</h5>
+          <p className="para">
+            I'm open to collaboration and connecting with like-minded
+            individuals. Feel free to reach out for discussions on coding,
+            technology, or potential projects.
+          </p>
+          <br></br>
+          <br></br>
+          <br></br>
+          <code className="para2">
+            Looking forward to the exciting journey ahead in the realm of
+            software development! 💞
+          </code>
         </div>
       </div>
     </>
